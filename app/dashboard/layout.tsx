3be811be01ae51af6584ec780f3a258846f78e63
Lou Schillaci <lou@delloop.com>
2025-11-21 13:18:46 +0000

    chore: Update version number in UI to 0.9901

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -176,7 +176,7 @@ export default function DashboardLayout({ children }: { children: ReactNode }) {
           
           {/* Version Number */}
           <div className="text-center mt-4 pb-2">
-            <span className="text-xs text-gray-400 font-mono">BETA V0.9800</span>
+            <span className="text-xs text-gray-400 font-mono">BETA V0.9901</span>
           </div>
         </nav>
       </aside>
